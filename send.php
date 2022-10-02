f725c7b780db6ec60be8f38579bb8aecc7504509
AlexanderGorin <72348529+ChipCh@users.noreply.github.com>
2022-10-02 03:28:28 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,35 @@
+<?php
+	
+	// Получаю данные с формы и сразу их обрабатываю
+
+	$login = $_POST['login'];
+	$login = htmlspecialchars($login);
+	$login = urldecode($login);
+	$login = trim($login);
+
+	$pass = $_POST['pass'];
+	$pass = htmlspecialchars($pass);
+	$pass = urldecode($pass);
+	$pass = trim($pass);
+
+	$email = $_POST['email'];
+	$email = htmlspecialchars($email);
+	$email = urldecode($email);
+	$email = trim($email);
+
+	// Отправляю данные на почту
+
+	if (mail(
+		"aleksandr-3565@mail.ru",
+		"Новое сообщение с сайта бистро 'Дежавю'",
+		"Логин: ".$login."\n".
+		"Пароль: ".$pass."\n".
+		"Email: ".$email."\n".
+		"Логин: ".$login,
+		"From: no-reply@mydomain.ru \r\n")) 
+		{
+			echo ('Письмо успешно отправлено!');
+	} else {
+		echo ('Заполните все поля');
+	}
+?>
